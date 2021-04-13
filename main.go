bd31e9dc873e588d745e7610a72700d833eca140
vipul patel <vipul.mindinventory@gmail.com>
2021-04-13 18:01:43 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/karmdip-mi/go-fitz"
+	"image/jpeg"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	var files []string
+
+	root := "pdf/"
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if filepath.Ext(path) == ".pdf" {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		doc, err := fitz.New(file)
+		if err != nil {
+			panic(err)
+		}
+		folder := strings.TrimSuffix(path.Base(file), filepath.Ext(path.Base(file)))
+
+		// Extract pages as images
+		for n := 0; n < doc.NumPage(); n++ {
+			img, err := doc.Image(n)
+			if err != nil {
+				panic(err)
+			}
+			err = os.MkdirAll("img/"+folder, 0755)
+			if err != nil {
+				panic(err)
+			}
+
+			f, err := os.Create(filepath.Join("img/"+folder+"/", fmt.Sprintf("image-%05d.jpg", n)))
+			if err != nil {
+				panic(err)
+			}
+
+			err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
+			if err != nil {
+				panic(err)
+			}
+
+			f.Close()
+
+		}
+	}
+}
